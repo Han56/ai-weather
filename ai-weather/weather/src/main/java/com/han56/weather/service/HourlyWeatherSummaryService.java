be8c831577eb30192cdb5e8a1739e6c446c986d4
Han56 <2279627931@qq.com>
2025-07-26 13:46:35 +0800

    [后端]拆分大模型模块到langchain模块

--- ai-weather/weather/src/main/java/com/han56/weather/service/HourlyWeatherSummaryService.java
+++
@@ -1,7 +0,0 @@
-package com.han56.weather.service;
-
-import org.springframework.web.context.request.async.DeferredResult;
-
-public interface HourlyWeatherSummaryService {
-    void getHourlyWeatherSummary(DeferredResult<String> result, String cityId);
-}
